ca6d9d3f405e042312e18093a8baec05bf3aa4a7
pooya-dev <pooya.jv@gmail.com>
2023-03-07 15:05:16 +0330

    example created

--- main.py
+++ main.py
@@ -1,1 +1,71 @@
-print('hello bot')
+import telegram
+from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
+
+# Create a Telegram bot instance
+bot = telegram.Bot(token='6134742025:AAEjpmRto-Sk-DdERreQ_KVr7S2Vj0bGFdk')
+
+# Define the start command handler
+def start(update, context):
+    context.bot.send_message(chat_id=update.effective_chat.id, text="Welcome to our shop! How can I assist you today?")
+
+# Define the help command handler
+def help(update, context):
+    context.bot.send_message(chat_id=update.effective_chat.id, text="Here are some available commands:\n/products - List available products\n/cart - View your shopping cart\n/checkout - Proceed to checkout")
+
+# Define the products command handler
+def products(update, context):
+    # Retrieve product information from database or API
+    products = [
+        {'name': 'Product 1', 'price': '$10'},
+        {'name': 'Product 2', 'price': '$20'},
+        {'name': 'Product 3', 'price': '$30'},
+    ]
+
+    # Send product information to user
+    product_list = "\n".join([f"{p['name']}: {p['price']}" for p in products])
+    context.bot.send_message(chat_id=update.effective_chat.id, text=f"Here are our available products:\n{product_list}")
+
+# Define the cart command handler
+def cart(update, context):
+    # Retrieve cart information from database or API
+    cart_items = [
+        {'name': 'Product 1', 'quantity': 2, 'price': '$10'},
+        {'name': 'Product 3', 'quantity': 1, 'price': '$30'},
+    ]
+
+    # Send cart information to user
+    cart_list = "\n".join([f"{p['name']}: {p['quantity']} x {p['price']}" for p in cart_items])
+    context.bot.send_message(chat_id=update.effective_chat.id, text=f"Here are the items in your cart:\n{cart_list}")
+
+# Define the checkout command handler
+def checkout(update, context):
+    # Retrieve cart information from database or API
+    cart_items = [
+        {'name': 'Product 1', 'quantity': 2, 'price': '$10'},
+        {'name': 'Product 3', 'quantity': 1, 'price': '$30'},
+    ]
+
+    # Calculate total cost
+    total_cost = sum([int(p['price'][1:]) * p['quantity'] for p in cart_items])
+
+    # Send checkout information to user
+    context.bot.send_message(chat_id=update.effective_chat.id, text=f"Your total cost is {total_cost}. Please proceed to payment.")
+
+# Define the unknown command handler
+def unknown(update, context):
+    context.bot.send_message(chat_id=update.effective_chat.id, text="Sorry, I didn't understand that command.")
+
+# Create an Updater instance and add handlers
+updater = Updater(token='your_bot_token', use_context=True)
+dispatcher = updater.dispatcher
+
+dispatcher.add_handler(CommandHandler('start', start))
+dispatcher.add_handler(CommandHandler('help', help))
+dispatcher.add_handler(CommandHandler('products', products))
+dispatcher.add_handler(CommandHandler('cart', cart))
+dispatcher.add_handler(CommandHandler('checkout', checkout))
+dispatcher.add_handler(MessageHandler(Filters.command, unknown))
+
+# Start the bot
+updater.start_polling()
+updater.idle()
